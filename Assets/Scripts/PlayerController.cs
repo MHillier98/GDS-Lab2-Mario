5ec63061e77eb85cb3529d7b2b5a992b2a46c73c
MHillier98 <26564949+MHillier98@users.noreply.github.com>
2021-03-08 16:45:29 +1100

    Code autoformat

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-
     private Rigidbody2D rb;
     private Animator animator;
 
@@ -36,13 +35,15 @@ public class PlayerController : MonoBehaviour
             }
             isGrounded = false;
         }
-        if(MushroomPickup)
+
+        if (MushroomPickup)
         {
             animator.SetBool("MushroomGet", true);
             BigMario = true;
             StartCoroutine(MushroomAnim());
         }
-        if(MarioDead)
+
+        if (MarioDead)
         {
             ResetLevel();
         }
@@ -59,11 +60,11 @@ public class PlayerController : MonoBehaviour
 
             rb.AddForce(MovementDir);
 
-            if(MovementDir.x > 0)
+            if (MovementDir.x > 0)
             {
                 Sprite.flipX = false;
             }
-            if(MovementDir.x < 0)
+            else if (MovementDir.x < 0)
             {
                 Sprite.flipX = true;
             }
@@ -89,36 +90,37 @@ public class PlayerController : MonoBehaviour
         {
             isGrounded = true;
         }
-        if(collider.tag == "Pickup")
+        else if (collider.tag == "Pickup")
         {
             MushroomPickup = true;
             Destroy(collider.gameObject);
         }
-        if (collider.tag == "OutOfBounds")
+        else if (collider.tag == "OutOfBounds")
         {
             MarioDead = true;
             //Debug.Log("Player dead");
         }
     }
-private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
         if (collision.collider.tag == "Shell" && collision.collider is BoxCollider2D)
         {
             collision.rigidbody.velocity = new Vector2(-10f, 0f);
-            
         }
         else if (collision.collider.tag == "Shell" && collision.collider is CircleCollider2D)
         {
             collision.rigidbody.velocity = new Vector2(10f, 0f);
         }
-      }
+    }
+
     IEnumerator MushroomAnim()
     {
         yield return new WaitForSeconds(1.0f);
         MushroomPickup = false;
     }
-   
+
     public void ResetLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
